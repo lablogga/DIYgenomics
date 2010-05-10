30cc207a38d6adf00638f53b71aeee39570e63ea
Marat Nepomnyashy <maratbn@gmail.com>
2010-05-09 17:57:23 -0700

    Moved the condition-querying code to the area where the results are being used.

--- gen_data.php
+++ gen_data.php
@@ -74,41 +74,6 @@
     require('database_opener.php');
     $DBConnect = openTheDatabase() or die ("<p>Unable to open the appropriate database.  Error code: " . mysql_connect_errno() . "</p>");
 
-    /*  QUERY THE DATABASE - QUERY 3(CONDITIONS LIST)
-            Returns a list like this:
-            1   Alzheimer's disease
-            2   Atrial fibrillation
-            3   Breast cancer
-            4   Celiac disease
-            5   Colorectal cancer
-            6   Crohn's disease
-            7   Diabetes (type 1)
-            8   Diabetes (type 2)
-            9   Glaucoma
-            10  Heart attack
-            11  Lung cancer
-            12  Lupus
-            13  Macular degeneration
-            14  Multiple sclerosis
-            15  Obesity
-            16  Prostate cancer
-            17  Psoriasis
-            18  Restless legs syndrome
-            19  Rheumatoid arthritis
-            20  Ulcerative colitis
-    */
-    $SQLstring3 = "SELECT 2_conditions.Primary, 2_conditions.Condition FROM 2_conditions;";
-
-    $QueryResult3 = mysql_query($SQLstring3)
-                        or die("<p>Unable to query a database table for conditions.  Error code: " . mysql_connect_errno() . "</p>");
-
-
-    //READ QUERY 3 RESULTS INTO ARRAY
-    // conditions holds rows {condition_ix, condition_name}
-    $Conditions = array();
-    while ($CondRow = mysql_fetch_array($QueryResult3)) {
-        $Conditions[] = $CondRow;
-    }
 ?>
 <table border='0' cellspacing='0' cellpadding='0' style='margin-left: 1.4in; margin-right: 1in;'>
     <tr>
@@ -127,6 +92,42 @@
                 <select name='condition' onchange='this.form.submit()'>
 
                     <?php
+                        /*  QUERY THE DATABASE - QUERY 3(CONDITIONS LIST)
+                                Returns a list like this:
+                                1   Alzheimer's disease
+                                2   Atrial fibrillation
+                                3   Breast cancer
+                                4   Celiac disease
+                                5   Colorectal cancer
+                                6   Crohn's disease
+                                7   Diabetes (type 1)
+                                8   Diabetes (type 2)
+                                9   Glaucoma
+                                10  Heart attack
+                                11  Lung cancer
+                                12  Lupus
+                                13  Macular degeneration
+                                14  Multiple sclerosis
+                                15  Obesity
+                                16  Prostate cancer
+                                17  Psoriasis
+                                18  Restless legs syndrome
+                                19  Rheumatoid arthritis
+                                20  Ulcerative colitis
+                        */
+                        $SQLstring3 = "SELECT 2_conditions.Primary, 2_conditions.Condition FROM 2_conditions;";
+
+                        $QueryResult3 = mysql_query($SQLstring3)
+                                            or die("<p>Unable to query a database table for conditions.  Error code: " . mysql_connect_errno() . "</p>");
+
+
+                        //READ QUERY 3 RESULTS INTO ARRAY
+                        // conditions holds rows {condition_ix, condition_name}
+                        $Conditions = array();
+                        while ($CondRow = mysql_fetch_array($QueryResult3)) {
+                            $Conditions[] = $CondRow;
+                        }
+
                         $COND = $_POST["condition"];
                         if ($COND == "") {
                           $COND="1";
