6aaa10ef001ec67f1a87d1a87a9a34b5277ec12e
Marat Nepomnyashy <maratbn@gmail.com>
2010-06-14 23:17:24 -0700

    Removed the 'debugAtAllCosts' setting from the 'release' branch.

--- gen_data.php
+++ gen_data.php
@@ -65,7 +65,7 @@
         <script
             type='text/javascript'
             src='/dojo-release-1.4.3/dojo/dojo.js'
-            djConfig="debugAtAllCosts: true, parseOnLoad: true, modulePaths:{'DIYgenomics':'/dojo/DIYgenomics/'}">
+            djConfig="parseOnLoad: true, modulePaths:{'DIYgenomics':'/dojo/DIYgenomics/'}">
         </script>
 
          <!-- Favicon Information -->
