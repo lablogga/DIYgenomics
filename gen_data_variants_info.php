51110925bd0fd119f0ff81aeff0ce13ad8c6955b
Melanie Swan <mxswan@gmail.com>
2011-01-28 14:28:06 -0300

    add 'Rank' as a display field

--- gen_data_variants_info.php
+++ gen_data_variants_info.php
@@ -94,6 +94,7 @@
                 ?>
                 <th><a href='http://www.ncbi.nlm.nih.gov/projects/SNP'>dbSNP (Nrml/Rsk)</a></th>
                 <th>Sample data</th>
+                <th>Rank</th>
             </tr>
             <?php
                 for ($i = 0; $i < count($mapDataCurrentCondition['variants']); $i++) {
@@ -157,7 +158,10 @@
                                     echo "<span style='color:$strColor2;'>" . htmlentities($mapDataCurrentCondition['variants_keyed'][$strVariant]['dbSNP_sample_2']) . "</span>";
                                 ?>
                             </td>
-                        </tr>
+                            <td>
+                            <?=htmlentities($mapDataCurrentCondition['variants_keyed'][$strVariant]['rank'])?>
+                            </td>
+                       </tr>
                     <?php
                 }
             ?>
