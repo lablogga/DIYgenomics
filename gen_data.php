908ce7f412a0fd84c0de68a65d12c94e73d33a01
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-06 20:11:53 -0700

    Merged in branch 'master' into 'release'.

--- gen_data.php
+++ gen_data.php
@@ -60,18 +60,7 @@
         <meta name="description" content="DIYgenomics Genome Health Risk Web Application" />
         <meta name="keywords" content="genomics, genome, personal genomes, personal genome, citizen science, science, technology, direct-to-consumer, consumer genomics, genomic testing, genetic testing, research" />
 
-        <!--  The following includes the h5ile HTML 5 File API helper library
-              that is used to load the user genome files. -->
-        <script type='text/javascript' src='/h5ile-release-0.1-min.js'>
-        </script>
-
-        <!--  The following includes the Dojo release 1.5.0 script from '/dojo-release-1.5.0/dojo/dojo.js'
-              and will access non-Dojo-specific modules from '/dojo/DIYgenomics/'. -->
-        <script
-            type='text/javascript'
-            src='/dojo-release-1.5.0/dojo/dojo.js'
-            djConfig="parseOnLoad: true, modulePaths:{'DIYgenomics':'/dojo/DIYgenomics/'}">
-        </script>
+        <?php require('common_js.php') ?>
 
          <!-- Favicon Information -->
         <link rel="shortcut icon" type="image/x-icon" href="http://www.diygenomics.org/images/favicon.ico">
