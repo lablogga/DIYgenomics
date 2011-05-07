63c5e0a07c6caf6f59dd9a4ae417f4d7d8f14218
Marat Nepomnyashy <maratbn@gmail.com>
2011-05-06 19:39:19 -0700

    Merged branch 'master' into 'pharma2'.

---
+++ common_js.php
@@ -0,0 +1,12 @@
+<!--  The following includes the h5ile HTML 5 File API helper library
+      that is used to load the user genome files. -->
+<script type='text/javascript' src='/h5ile-release-0.1-min.js'>
+</script>
+
+<!--  The following includes the Dojo release 1.5.0 script from '/dojo-release-1.5.0/dojo/dojo.js'
+      and will access non-Dojo-specific modules from '/dojo/DIYgenomics/'. -->
+<script
+    type='text/javascript'
+    src='/dojo-release-1.5.0/dojo/dojo.js'
+    djConfig="debugAtAllCosts: true, parseOnLoad: true, modulePaths:{'DIYgenomics':'/dojo_pharma/DIYgenomics/'}">
+</script>
